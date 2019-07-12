31c04c21f51865ba8f71ab1441905c200471a414
kschwartz439 <kschwartz439@gmail.com>
2019-07-12 11:37:23 -0500

    Setting up ConnectionUtils

--- src/main/java/com/revature/connectionutils/ConnectionUtils.java
+++ src/main/java/com/revature/connectionutils/ConnectionUtils.java
@@ -6,24 +6,38 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.*;
+import java.util.Scanner;
 
+//Connection with the server.
 public class ConnectionUtils{
-    try (ServerSocket sc = new ServerSocket(8080)){
-        while (true){
-            try (Socket socket = sc.accept();
-                OutputStream os = socket.getOutputStream()){
-                BufferedReader bread = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                String line = bread.readLine();
-                while (!line.isEmpty()){
-                    System.out.println(line);
-                    bread.readLine();
-                }
-                String response = "HTTP:/1.1 200 OK\r\nContent-Type: application.json\r\n\r\n{'name':'placeholder'";
-                os.write(response.getBytes("UTF-8"));
+    public ConnectionUtils(){
+        String url = "jdbc:postgresql://192.168.99.100:5432/postgres";
+        String user = "KellyCr";
+        String password = "p4ssw0rd";
+        String query;
+
+        String admin_id;
+
+        try (Connection connection = DriverManager.getConnection(url, user, password)){
+            Scanner scanner = new Scanner(System.in);
+
+            while (true){
+                PreparedStatement pstatement = connection.prepareCall("get_admin_id(admin_id)");
+                pstatement.setString(/*# of rows*/, admin_id);
+                //if ResultSet != null, check password
+                //if password != adminPass, ask for a correct username/password
+                PreparedStatement eStatement = connection.prepareCall("get_employee_id(employee_id)");
+                //if get_employee_id != null, check password
+                //if password != employeePass, ask for a correct username/password
+                PreparedStatement cStatement = connection .prepareCall("get_customer_id(customer_id)");
+                //if get_customer_id != null, check password
+                // if password != customer pass, ask for a correct username/password
+                //if all = null, ask for a correct username/password
             }
         }
+        catch (SQLException e){
+
+        }
     }
-    catch (IOException ex){
-        
-}
 }
