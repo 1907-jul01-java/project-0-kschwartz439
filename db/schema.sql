b84568186676e4913c93dcb6011fe8ed7acdfb67
kschwartz439 <kschwartz439@gmail.com>
2019-07-12 09:04:18 -0500

    Added functions

--- db/schema.sql
+++ db/schema.sql
@@ -8,7 +8,24 @@ create table customers (id serial primary key, firstName varchar not null, lastN
 create table employees (id serial primary key, firstName varchar not null, lastName varchar not null, employeeId varchar not null, employeePass varchar not null);
 create table admins (id serial primary key, firstName varchar not null, lastName varchar not null, adminId varchar not null, adminPass varchar not null);
 create table creator (id serial primary key, pass int not null, creatorId varchar not null);
-create or replace function get_admin_id(adminId varchar) returns integer AS
+
+--Get PL/SQL language to run this line
+create or replace function get_admin_id(admin_user varchar) returns integer AS
 $$
     SELECT id FROM admins WHERE admin_id = admin_user;
+$$language sql;
+
+create or replace function get_creator_id(creator_user varchar) returns integer AS
+$$
+    SELECT id FROM admins WHERE creator_id = creator_user;
+$$language sql;
+
+create or replace function get_employee_id(employee_user varchar) returns integer AS
+$$
+    SELECT id FROM admins WHERE employee_id = employee_user;
+$$language sql;
+
+create or replace function get_customer_id(customer_user varchar) returns integer AS
+$$
+    SELECT id FROM admins WHERE customer_id = customer_user;
 $$language sql;
